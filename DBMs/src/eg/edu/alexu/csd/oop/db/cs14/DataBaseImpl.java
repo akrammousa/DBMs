10f7aab5c5ad17a9eadb014fc4eae99caa3d2515
LENOVO <LENOVO@DESKTOP-5AUS6LL>
2017-11-23 12:27:04 +0200

    Data base impel.

---
+++ DBMs/src/eg/edu/alexu/csd/oop/db/cs14/DataBaseImpl.java
@@ -0,0 +1,42 @@
+package eg.edu.alexu.csd.oop.db.cs14;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import eg.edu.alexu.csd.oop.db.Database;
+
+public class DataBaseImpl implements Database {
+
+	@Override
+	public String createDatabase(String databaseName, boolean dropIfExists) {
+		final File f = new File(databaseName);
+		if (f.exists()) {
+			if (dropIfExists) {
+				f.delete();
+				f.mkdir();
+			} else {
+				f.mkdir();
+			}
+		}
+		return f.getAbsolutePath();
+	}
+
+	@Override
+	public boolean executeStructureQuery(String query) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[][] executeQuery(String query) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int executeUpdateQuery(String query) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
