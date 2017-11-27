1e6123ef2c496755fcec7fc1b14cc92846d63519
youmna_dwidar <youmnadwidar@hotmail.com>
2017-11-27 02:25:24 +0200

    TwoString class.

---
+++ DBMs/src/eg/edu/alexu/csd/oop/db/cs14/TwoStrings.java
@@ -0,0 +1,20 @@
+package eg.edu.alexu.csd.oop.db.cs14;
+
+public class TwoStrings {
+	
+	private String string ;
+	private String value ;
+	public String getString() {
+		return string;
+	}
+	public String getValue() {
+		return value;
+	}
+	public TwoStrings(String string, String value) {
+		this.string = string;
+		this.value = value;
+	}
+	
+	
+
+}
